a7f96f0fbf4718f1402e4fce527b790ee0832eb9
Dennis Schreur <dennis@tdschreur.nl>
2022-10-12 09:13:06 +0200

    werkt! een beetje dan...

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "Adafruit_TCS34725.h"
 #include <ArduinoJson.h>
 #include <az_core.h>
+#include "ESP32Servo.h"
 
 #include "seriallogger.h"
 #include "timeconfig.h"
@@ -13,8 +14,13 @@
 char buffer[1024];
 float r, g, b;
 
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
+Servo myservo; // create servo object to control a servo
 
+// Integration time en gain hebben veel invloed op de snelheid en nauwkeurigheid van de sensor, deze beetje tweaken voor het beste resultaat
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_24MS, TCS34725_GAIN_1X);
+
+// Dit zijn de marge's van de sensor.
+// red_min, red_max, green_min, green_max, blue_min, blue_max
 MandM *red = new MandM(160, 170, 45, 55, 25, 35);
 MandM *geel = new MandM(120, 130, 80, 95, 20, 35);
 MandM *groen = new MandM(75, 85, 110, 120, 40, 50);
@@ -25,56 +31,75 @@ MandM *bruin = new MandM(110, 120, 80, 90, 40, 50);
 static void
 receivedCallback(String payload)
 {
-  // todo
+  // todo => M&M laten valleen wanneer het bakje klaar staat
 }
 
 static void getRGBColors(az_span payload, az_span *out_payload)
 {
+  // draaien maar
+  myservo.write(86);
+
+  String kleur;
+  bool colorFound = false;
+
+  // WHOOOOOOOOOOOOOOOOOOOT Lekker ranzige hackaton code!
+  while (!colorFound)
+  {
+
+    tcs.getRGB(&r, &g, &b);
+
+    snprintf(buffer, sizeof(buffer), "R: %.0f G: %.0f B: %.0f", r, g, b);
+    Logger.Info(buffer);
+
+    if (red->IsMatch(r, g, b))
+    {
+      Logger.Info("ROOD");
+      kleur = "rood";
+      colorFound = true;
+    }
+    else if (geel->IsMatch(r, g, b))
+    {
+      Logger.Info("GEEL");
+      kleur = "geel";
+      colorFound = true;
+    }
+    else if (groen->IsMatch(r, g, b))
+    {
+      Logger.Info("GROEN");
+      kleur = "groen";
+      colorFound = true;
+    }
+    else if (oranje->IsMatch(r, g, b))
+    {
+      Logger.Info("ORANJE");
+      kleur = "oranje";
+      colorFound = true;
+    }
+    else if (blauw->IsMatch(r, g, b))
+    {
+      Logger.Info("BLAUW");
+      kleur = "blauw";
+      colorFound = true;
+    }
+    else if (bruin->IsMatch(r, g, b))
+    {
+      Logger.Info("BRUIN");
+      kleur = "bruin";
+      colorFound = true;
+    }
+    else
+    {
+      // Logger.Error("KLEUR NIET KUNNEN BEPALEN");
+      kleur = "leeg";
+    };
+  }
+
+  // servo stoppen
+  myservo.write(90);
+
   // String json;
   StaticJsonDocument<256> document;
   char buffer[256];
-
-  tcs.getRGB(&r, &g, &b);
-
-  snprintf(buffer, sizeof(buffer), "R: %.0f G: %.0f B: %.0f", r, g, b);
-  Logger.Info(buffer);
-
-  String kleur;
-  if (red->IsMatch(r, g, b))
-  {
-    Logger.Info("ROOD");
-    kleur = "rood";
-  }
-  else if (geel->IsMatch(r, g, b))
-  {
-    Logger.Info("GEEL");
-    kleur = "geel";
-  }
-  else if (groen->IsMatch(r, g, b))
-  {
-    Logger.Info("GROEN");
-    kleur = "groen";
-  }
-  else if (oranje->IsMatch(r, g, b))
-  {
-    Logger.Info("ORANJE");
-    kleur = "oranje";
-  }
-  else if (blauw->IsMatch(r, g, b))
-  {
-    Logger.Info("BLAUW");
-    kleur = "blauw";
-  }
-  else if (bruin->IsMatch(r, g, b))
-  {
-    Logger.Info("BRUIN");
-    kleur = "bruin";
-  }
-  else
-  {
-    Logger.Error("KLEUR NIET KUNNEN BEPALEN");
-    kleur = "leeg";
-  };
 
   document["red"] = r;
   document["green"] = g;
@@ -87,6 +112,12 @@ static void getRGBColors(az_span payload, az_span *out_payload)
   payload = az_span_copy(payload, az_span_create_from_str(buffer));
   payload = az_span_copy_u8(payload, '\0');
   *out_payload = az_span_slice(original_payload, 0, az_span_size(original_payload) - az_span_size(payload) - 1);
+
+  // even wachten en dan de M7M weer doordraaien
+  // eigenlijk willen we dit niet.
+  // We willen een cloud-to-device message wanneer het juiste bakje klaar staat.
+  sleep(6);
+  myservo.write(86);
 }
 
 void setup(void)
@@ -94,6 +125,8 @@ void setup(void)
   WifiHelper.InitializeWifi();
 
   TimeHelper.InitializeTime();
+
+  myservo.attach(27); // attaches the servo on pin 25 to the servo object
 
   Serial.begin(115200);
 
